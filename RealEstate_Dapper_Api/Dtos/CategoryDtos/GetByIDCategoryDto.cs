24f0a0d2ba5507039788ddea98091d2bbf81d487
Mert Öden <93067736+mertoden@users.noreply.github.com>
2024-12-15 19:49:32 +0300

    add category CRUD processes (8,9)

--- RealEstate_Dapper_Api/Dtos/CategoryDtos/GetByIDCategoryDto.cs
+++ RealEstate_Dapper_Api/Dtos/CategoryDtos/GetByIDCategoryDto.cs
@@ -1,9 +1,9 @@
 ﻿namespace RealEstate_Dapper_Api.Dtos.CategoryDtos
 {
-    public class UpdateCategoryDto
+    public class GetByIDCategoryDto
     {
         public int CategoryID { get; set; }
         public string CategoryName { get; set; }
         public bool CategoryStatus { get; set; }
-    }  
+    }
 }
