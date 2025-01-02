c021076aca2082ac165466a7634364a5470a7409
Mert Öden <93067736+mertoden@users.noreply.github.com>
2025-01-02 18:56:08 +0300

    Add Products Api , add menu page

---
+++ RealEstate_Dapper_Api/Dtos/ProductDtos/ResultProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate_Dapper_Api.Dtos.ProductDtos
+{
+    public class ResultProductDto
+    {
+        public int SQ_ProductID { get; set; }
+        public string CD_Title { get; set; }
+        public string DC_Price { get; set;}
+        public string CD_City { get; set;}
+        public string CD_District { get; set;}
+        public int RF_ProductCategory {  get; set;}
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.ProductDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.ProductRepository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Context _context;
+
+        public ProductRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            string query = "Select * From Product";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync()
+        {
+            string query = "Select SQ_ProductID,CD_Title,DC_Price,CD_City,CD_District,CategoryName From Product inner join Category on Product.RF_ProductCategory=Category.CategoryID";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductWithCategoryDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
